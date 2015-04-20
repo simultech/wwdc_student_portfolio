c344a80ca65a3ee3a2fbea0a65307d58733519ff
Andrew Dekker <simultech@gmail.com>
2015-04-20 17:03:57 +1000

    Initial Commit

---
+++ wwdc_student_portfolio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wwdc_student_portfolio
+//
+//  Created by Andrew Dekker on 20/04/2015.
+//  Copyright (c) 2015 Andrew Dekker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
