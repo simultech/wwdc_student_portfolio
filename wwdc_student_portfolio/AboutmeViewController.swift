15d975aa2eac323b7b0c5b00f2638ae419466cd8
nurmerey <nurmerey@Nurmereys-MacBook-Pro.local>
2015-04-22 00:34:37 +1000

    vertical button

--- wwdc_student_portfolio/AboutmeViewController.swift
+++ wwdc_student_portfolio/AboutmeViewController.swift
@@ -9,8 +9,21 @@
 import UIKit
 
 class AboutmeViewController : UIViewController {
+    @IBOutlet weak var aboutMeButton: UIButton!
+    @IBOutlet weak var aboutMeLabel: UILabel!
+
+    @IBAction func aboutMeButtonClick(sender: UIButton) {
+        print("about me button click")
+    }
+
+    override func viewDidLoad() {
+    print("about me page")
+        //aboutMeButton.titleLabel?.text = "hello"
+        aboutMeButton.transform = CGAffineTransformMakeRotation(CGFloat(-M_PI_2))
+       
+    }
     func setup(){
-        print("about me page")
+
     }
 
 
